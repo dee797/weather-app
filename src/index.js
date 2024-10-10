80ed18675454b1c937c202e3dfad9bd9ec362536
dee <178827694+dee797@users.noreply.github.com>
2024-10-10 17:15:12 -0600

    Add logic for displaying data to user

--- src/index.js
+++ src/index.js
@@ -4,28 +4,67 @@ import str from '../storage';
 
 
 async function sendRequest(location) {
+
     let query = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-    query += `${location}?key=${str}`;
+    query += `${location}?key=${str}&contentType=json`;
 
     try {
+
         const response = await fetch(`${query}`, { mode: 'cors' });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
         const data = await response.json();
         return data;
+
     } catch (error) {
-        console.error(error);
-        return '';
+
+        console.error(error.message);
+        alert('Invalid location. Please try again.');
+
     }
 }
 
 
 const domHandler = (function () {
-    const form = document.querySelector('#weather');
 
-    form.addEventListener('submit', (e) => {
-        const data = Object.fromEntries(new FormData(form));
+    const form = document.querySelector('#weather');
+    const changeBtn = document.querySelector('#changeDeg');
+
+    const displayData = (data) => {
+        const table = document.querySelector('#table');
+        const caption = document.querySelector('#caption');
+        const conditions = document.querySelector('#conditions');
+        const currentTemp = document.querySelector('#currentTemp');
+        const feelsLike = document.querySelector('#feelsLike');
+        const minTemp = document.querySelector('#minTemp');
+        const maxTemp = document.querySelector('#maxTemp');
+
+        caption.textContent = data.resolvedAddress;
+        conditions.textContent = data.currentConditions.conditions;
+        currentTemp.textContent = data.currentConditions.temp;
+        feelsLike.textContent = data.currentConditions.feelslike;
+        minTemp.textContent = data.days[0].tempmin;
+        maxTemp.textContent = data.days[0].tempmax;
+
+        table.style.display = 'block';
+    };
+
+
+    form.addEventListener('submit', async (e) => {
+        const input = Object.fromEntries(new FormData(form));
         e.preventDefault();
-        const weatherData = sendRequest(data.location);
-        console.log(weatherData);
+        const weatherData = await sendRequest(input.location);
+        weatherData.then(() => {
+            displayData(weatherData);
+        });
     });
+
+
+    changeBtn.addEventListener('click', () => {
+
+    });
+
 }());
 
