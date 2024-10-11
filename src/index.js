df856bee8c8f7055b98f26b0b434e2d755bb9503
dee <178827694+dee797@users.noreply.github.com>
2024-10-11 11:44:29 -0600

    Add logic for fetching data in deg C, deg F

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
-// Use await any time a Promise is returned (promises are returned w fetch, .json())
-
+// Use await any time a Promise is returned (promises are returned w fetch, .json(), async func)
+import './styles.css';
 import str from '../storage';
 
 
 async function sendRequest(location) {
 
-    let query = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-    query += `${location}?key=${str}&contentType=json`;
+    const query = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+    const queryFahr = `${query}${location}?unitGroup=us&key=${str}&contentType=json`;
+    const queryCel = `${query}${location}?unitGroup=metric&key=${str}&contentType=json`;
 
     try {
 
-        const response = await fetch(`${query}`, { mode: 'cors' });
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+        const responseFahr = await fetch(`${queryFahr}`, { mode: 'cors' });
+        if (!responseFahr.ok) {
+            throw new Error(`Response status: ${responseFahr.status}`);
         }
 
-        const data = await response.json();
-        return data;
+        const responseCel = await fetch(`${queryCel}`, { mode: 'cors' });
+        const dataFahr = await responseFahr.json();
+        const dataCel = await responseCel.json();
+        return [dataFahr, dataCel];
 
     } catch (error) {
 
-        console.error(error.message);
-        alert('Invalid location. Please try again.');
-
+        console.error(error);
+        alert('Location not found. Please try again.');
     }
 }
 
@@ -31,6 +33,7 @@ const domHandler = (function () {
 
     const form = document.querySelector('#weather');
     const changeBtn = document.querySelector('#changeDeg');
+    let weatherData;
 
     const displayData = (data) => {
         const table = document.querySelector('#table');
@@ -55,15 +58,27 @@ const domHandler = (function () {
     form.addEventListener('submit', async (e) => {
         const input = Object.fromEntries(new FormData(form));
         e.preventDefault();
-        const weatherData = await sendRequest(input.location);
-        weatherData.then(() => {
-            displayData(weatherData);
-        });
+        const data = await sendRequest(input.location);
+        if (data) {
+            weatherData = data;
+            displayData(weatherData[0]);
+            changeBtn.style.display = 'block';
+            changeBtn.value = 'f';
+            changeBtn.textContent = 'Change to Celsius';
+        }
     });
 
 
     changeBtn.addEventListener('click', () => {
-
+        if (changeBtn.value === 'f') {
+            displayData(weatherData[1]);
+            changeBtn.value = 'c';
+            changeBtn.textContent = 'Change to Fahrenheit';
+        } else {
+            displayData(weatherData[0]);
+            changeBtn.value = 'f';
+            changeBtn.textContent = 'Change to Celsius';
+        }
     });
 
 }());
